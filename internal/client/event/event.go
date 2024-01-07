e71214beea64abb41ae7ea13cf4fb9ec33a9de84
Antonov Denis <den_ru1982@mail.ru>
2024-01-07 20:27:00 +0300

    Add struct event

---
+++ internal/client/event/event.go
@@ -0,0 +1,19 @@
+package event
+
+import (
+	"encoding/json"
+
+	"github.com/adminsemy/WebSocket/internal/client"
+)
+
+type Event struct {
+	Type    string          `json:"type"`
+	Payload json.RawMessage `json:"payload"`
+}
+
+type EventHandler func(event *Event, c *client.WebSocketClient) error
+
+type SendMessageEvent struct {
+	Message string `json:"message"`
+	From    string `json:"from"`
+}

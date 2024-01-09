be554f43c18c41c623150b7416fb61022d02d77f
Antonov Denis <den_ru1982@mail.ru>
2024-01-09 14:17:53 +0300

    Add by webSocketClient even field  and methods from event

--- internal/server/handler/websocket.go
+++ internal/server/handler/websocket.go
@@ -1,11 +1,14 @@
 package handler
 
 import (
+	"errors"
+	"fmt"
 	"log/slog"
 	"net/http"
 	"sync"
 
 	"github.com/adminsemy/WebSocket/internal/client"
+	"github.com/adminsemy/WebSocket/internal/client/event"
 	"github.com/gorilla/websocket"
 )
 
@@ -14,14 +17,35 @@ type Manager struct {
 	delete    chan *client.WebSocketClient
 	messageCh chan []byte
 	sync.RWMutex
+	handlers map[string]event.EventHandler
 }
 
 func NewManager() *Manager {
-	return &Manager{
+	m := &Manager{
 		clients:   make(map[*client.WebSocketClient]bool),
 		delete:    make(chan *client.WebSocketClient),
 		messageCh: make(chan []byte),
+		handlers:  make(map[string]event.EventHandler),
 	}
+
+	return m
+}
+
+func (m *Manager) SetupHandlers() {
+	m.handlers[event.EventSendMessage] = func(e *event.Event, c *client.WebSocketClient) error {
+		fmt.Println(e)
+		return nil
+	}
+}
+
+func (m *Manager) routeEvent(e event.Event, c *client.WebSocketClient) error {
+	if handler, ok := m.handlers[e.Type]; ok {
+		if err := handler(&e, c); err != nil {
+			return err
+		}
+		return nil
+	}
+	return errors.New("event handler not found")
 }
 
 func (m *Manager) ServeWS(w http.ResponseWriter, r *http.Request) {
